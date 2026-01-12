9cbf3c7a351b54128689b52ae69b05c2d40c9cd2
Lucas <falla.urluc24@triana.salesianos.edu>
2026-01-12 15:02:06 +0100

    Borrar usuario por ID y editar usuario por ID implementados.

--- FastAPI-CRUD/main.py
+++ FastAPI-CRUD/main.py
@@ -22,6 +22,14 @@ db: List[User] = [
         gender = Gender.male,
         roles = [Role.admin, Role.user]
     ),
+    User(
+        id = UUID("6008d9ca-f63e-47f0-b75e-8699105c222b"),
+        first_name = "ANA",
+        last_name = "TEMPLETON",
+        nickname = "annaPle",
+        gender = Gender.female,
+        roles = [Role.editor]
+    ),
 ]
 
 @app.get("/")
@@ -43,3 +51,26 @@ async def get_user_by_id(user_id: UUID):
 async def create_user(user: User):
     db.append(user)
     return {"message": "Se ha creado un nuevo usuario correctamente."}
+
+@app.delete("/api/v1/user/{user_id}")
+async def delete_user_by_id(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return {"message": "Se ha borrado el usuario correctamente."}
+    raise HTTPException(status_code=404, detail="Usuario no encontrado.")
+
+@app.patch("/api/v1/user/{user_id}")
+async def edit_user_by_id(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.nickname is not None:
+                user.nickname = user_update.nickname
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return {"message": "El usuario ha sido actualizado correctamente."}
+    raise HTTPException(status_code=404, detail="Usuario no encontrado.")
